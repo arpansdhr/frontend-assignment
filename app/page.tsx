e212fe48ceb6c1657c1b5b4d70ddda990950cb95
Arpan Sutradhar <sendmail2arpansutradhar@gmail.com>
2024-11-27 09:49:21 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,11 @@ import RandomCatsClient from "./components/RandomCatsClient";
 
 export default function Home() {
   return (
-    <main className="container mx-auto px-4 py-8">
+    <main className="container mx-auto">
       <h1 className="text-4xl font-bold text-center mb-8">Welcome to our Cat Gallery!</h1>
       <RandomCatsClient />
       <p className="text-lg text-center">Discover cute and random cat images every time you visit.</p>
+      <footer className="text-center text-black py-1 bg-white">Made with &#10084; by Arpan Sutradhar</footer>
     </main>
   );
 }
